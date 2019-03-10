59507cbf0b915014d8723c90250117dbfed38e38
tranceyos2419 <tranceyos2419@gmail.com>
2019-03-10 20:50:10 +0900

    add useReducer

--- src/Hooks.js
+++ src/Hooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import Axios from "axios";
 import { useTodos } from "./hooks/todos";
 
@@ -9,25 +9,43 @@ import { useTodos } from "./hooks/todos";
 //* shouldComponentUpdate => React.memo(function,(prevProps,nextProps)=>{return false})
 
 //? .bind()
-//? useReducer
 //? useContext
 
 //* React.memo() => Stop rendering component unless passed props or state inside of the component are changed
+
+const initialState = { count: 1 };
+const INCREMENT = "INCREMENT";
+const RESET = "RESET";
+
+const init = () => {
+  return initialState;
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case RESET:
+      return init(action.payload);
+    default:
+      return state;
+  }
+};
 
 export default function Hooks() {
   const [title, setTitle] = useState("Hooks");
   const [words, setWords] = useState("");
   const [MyTodo, setMyTodo] = useState([]);
-  const [Counter, setCounter] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState, init);
   const Todos = useTodos([]);
 
   useEffect(() => {
-    Axios.get(`https://jsonplaceholder.typicode.com/todos/${Counter}`)
+    Axios.get(`https://jsonplaceholder.typicode.com/todos/${state.count}`)
       .then(res => {
         setMyTodo([...MyTodo, res.data]);
       })
       .catch(e => console.log(e));
-  }, [Counter]);
+  }, [state.count]);
 
   console.log(`my todo ${JSON.stringify(MyTodo)}`);
   return (
@@ -39,8 +57,11 @@ export default function Hooks() {
         onChange={e => setWords(e.target.value)}
       />
       <br />
-      <p>Counter :{Counter}</p>
-      <button onClick={() => setCounter(Counter + 1)}>Increment</button>
+      <p>Counter :{state.count}</p>
+      <button onClick={() => dispatch({ type: INCREMENT })}>Increment</button>
+      <button onClick={() => dispatch({ type: RESET, payload: 1 })}>
+        Reset
+      </button>
       <br />
       <h3>My Todo list</h3>
       <ul>
